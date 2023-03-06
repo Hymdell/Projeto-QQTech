ffb95cfcca8004c026c88f0953b0d122710ced5e
Cristopher Machado <cristopherpiussi@gmail.com>
2023-03-06 01:41:20 -0300

    criação do popup de cadastro e os respectivos popups de confirmação e negação da ação

--- site/js/dashboardAdmin.js
+++ site/js/dashboardAdmin.js
@@ -1,3 +1,41 @@
 function Sair() {
   window.location.href = "/site/html/index.html";
 }
+
+const btnAbrirPopup = document.getElementById("btnAbrirPopup");
+const btnFecharPopup = document.getElementById("btnFecharPopup");
+const btnFecharPopupConfirmacao = document.getElementById("btnFecharPopupConfirmacao");
+const btnFecharPopupNegacao = document.getElementById("btnFecharPopupNegacao");
+const btnCadastrar = document.getElementById("btnCadastrar");
+const botaoFechar = document.getElementById('fecharPopup');
+const popup = document.getElementById("popup");
+const confpopup = document.getElementById('confirmacaoPopup');
+const negpopup = document.getElementById('negacaoPopup');
+
+btnAbrirPopup.addEventListener("click", () => {
+  popup.style.display = "block";
+  popup.classList.add("active");
+});
+
+btnFecharPopup.addEventListener("click", () => {
+  popup.style.display = "none";
+  popup.classList.remove("active");
+});
+
+btnCadastrar.addEventListener("click", () => {
+  popup.style.display = "none";
+  popup.classList.remove("active");
+  confpopup.style.display = "block";
+});
+
+botaoFechar.addEventListener('click', () => {
+  confpopup.style.display = 'none';
+});
+
+btnFecharPopupConfirmacao.addEventListener("click", () => {
+  confpopup.style.display = "none";
+});
+
+btnFecharPopupNegacao.addEventListener("click", () => {
+  confpopup.style.display = "none";
+});
